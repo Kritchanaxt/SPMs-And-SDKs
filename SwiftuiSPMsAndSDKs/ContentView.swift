c5e35a5f99d12832d6b6716900b862d802b66a0e
Kritchanaxt <kritchanatweve@gmail.com>
2024-07-10 18:56:53 +0700

    Initial Commit

---
+++ SwiftuiSPMsAndSDKs/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftuiSPMsAndSDKs
+//
+//  Created by Kritchanat on 10/7/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftuiSPMsAndSDKs/SwiftuiSPMsAndSDKsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiSPMsAndSDKsApp.swift
+//  SwiftuiSPMsAndSDKs
+//
+//  Created by Kritchanat on 10/7/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiSPMsAndSDKsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
